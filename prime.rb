ec5ebaf460fa15069bcbfc2f2fbe9c1170ee9eae
William Stevens <wstevens.hornet@gmail.com>
2019-10-13 05:17:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(num)
-  range = 0
+  i = 0
   if num > 1
-    range = (2..num-1).to_a
-    range.none? do |check| 
+    i = (2..num-1).to_a
+    i.none? do |check| 
       num % check == 0
     end
   else
