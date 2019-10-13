7995f27898cdddac21c2960ba5e88d646a5700e9
William Stevens <wstevens.hornet@gmail.com>
2019-10-13 05:18:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,7 @@ def prime?(num)
   i = 0
   if num > 1
     i = (2..num-1).to_a
-    i.none? do |check| 
-      num % check == 0
-    end
+    i.none?{ |check| num % check == 0}
   else
     false
   end
